5c7994e4a2e8c069e8549a32a6c69c8609cd1615
sscholfield0409 <162228341+sscholfield0409@users.noreply.github.com>
2024-09-28 14:22:53 -0500

    update Phys and compSci
    setting specifics for the child classes, and was able to set the same types of properties as the Teacher class

--- src/com/coderscampus/lesson1/CompSci.java
+++ src/com/coderscampus/lesson1/CompSci.java
@@ -1,5 +1,10 @@
 package com.coderscampus.lesson1;
 
-public class CompSci extends Teacher{
+public class CompSci extends Teacher {
+	
+	public CompSci () {
+		this.setClasses("COMP 101, COMP 201");
+		this.setLevelOfEducation("Masters of Computer Science");
+	}
 
 }
--- src/com/coderscampus/lesson1/PhysicsTeacher.java
+++ src/com/coderscampus/lesson1/PhysicsTeacher.java
@@ -1,5 +1,10 @@
 package com.coderscampus.lesson1;
 
 public class PhysicsTeacher extends Teacher {
+	
+	public PhysicsTeacher () {
+		this.setClasses("PHYS 101, PHYS 201");
+		this.setLevelOfEducation("Masters of Science");
+	}
 
 }
