107e73bb7212407e0caca79252743bb802522efe
sscholfield0409 <162228341+sscholfield0409@users.noreply.github.com>
2024-09-28 13:57:48 -0500

    update teacher.java
    creating getters and setters

--- src/com/coderscampus/lesson1/Teacher.java
+++ src/com/coderscampus/lesson1/Teacher.java
@@ -1,5 +1,20 @@
 package com.coderscampus.lesson1;
 
 public class Teacher {
-
+	private String classes;
+	private String levelOfEducation;
+	
+	public String getClasses() {
+		return classes;
+	}
+	public void setClasses(String classes) {
+		this.classes = classes;
+	}
+	public String getLevelOfEducation() {
+		return levelOfEducation;
+	}
+	public void setLevelOfEducation(String levelOfEducation) {
+		this.levelOfEducation = levelOfEducation;
+	}
+	
 }
