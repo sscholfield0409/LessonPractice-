6660840766ee9a21f65dd287c5ea5e2ac973c8ca
sscholfield0409 <162228341+sscholfield0409@users.noreply.github.com>
2024-09-30 08:33:05 -0500

    Update Lesson1Applicaiton dataType
    by changing the data type of PhysicsTeacher physicsTeacher = new PhysicsTeacher to just Teacher physicsTeacher = new PhysicsTeacher, I'm able to determine that the console output is still the same due to the override method made in the PhysicsTeacher.java class

--- src/com/coderscampus/lesson1/Lesson1Application.java
+++ src/com/coderscampus/lesson1/Lesson1Application.java
@@ -3,7 +3,7 @@ package com.coderscampus.lesson1;
 public class Lesson1Application {
 
 	public static void main(String[] args) {
-		PhysicsTeacher physicsTeacher = new PhysicsTeacher();
+		Teacher physicsTeacher = new PhysicsTeacher();
 		
 		System.out.println(physicsTeacher.getClasses());
 		System.out.println(physicsTeacher.getLevelOfEducation());
