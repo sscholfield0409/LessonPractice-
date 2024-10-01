efd0875b3226757310b438937beafce5dd0242b0
sscholfield0409 <162228341+sscholfield0409@users.noreply.github.com>
2024-10-01 09:50:14 -0500

    Update PhysicsTeacher.java
    removed override of the overload because this lesson is about overload not overriding

--- src/com/coderscampus/lesson1/PhysicsTeacher.java
+++ src/com/coderscampus/lesson1/PhysicsTeacher.java
@@ -1,22 +1,17 @@
 package com.coderscampus.lesson1;
 
 public class PhysicsTeacher extends Teacher {
-	
-	public PhysicsTeacher () {
+
+	public PhysicsTeacher() {
 		System.out.println("Inside the PhysicsTeacher constructor");
 		this.setClasses("PHYS 101, PHYS 201");
 		this.setLevelOfEducation("Masters of Science");
 	}
-	
+
 	@Override
-	public void teach () {
+	public void teach() {
 		System.out.println("I'm a physics teacher and I'm now teaching physics");
 	}
-	
-	/*
-	 * @Override public void teach (String teacherName) {
-	 * System.out.println("I'm a physics teacher called " + teacherName +
-	 * " and I'm now teaching physics"); }
-	 */
-	
+
+
 }
