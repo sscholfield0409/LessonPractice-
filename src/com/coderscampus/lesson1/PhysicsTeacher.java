ee0bf233603c18bff906dc5f423273539788b6e3
sscholfield0409 <162228341+sscholfield0409@users.noreply.github.com>
2024-10-01 09:38:23 -0500

    Update PhysicsTeacher.java String for overLoad method
    altered the String for the overload teach method in PhysicsTeacher.java

--- src/com/coderscampus/lesson1/PhysicsTeacher.java
+++ src/com/coderscampus/lesson1/PhysicsTeacher.java
@@ -14,7 +14,7 @@ public class PhysicsTeacher extends Teacher {
 	}
 	
 	public void teach (String teacherName) {
-		System.out.println("I'm a physics teacher and I'm now teaching physics");
+		System.out.println("I'm a physics teacher called" + teacherName + "and I'm now teaching physics");
 	}
 
 }
